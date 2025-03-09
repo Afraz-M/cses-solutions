19ad5324a1e5b7531ef90343eaf7a7a1ff5c10b5
Afraz M <afrazm2005@gmail.com>
2025-03-09 23:39:59 +0530

    path queries II half done:

--- trees/pathQueriesII.cpp
+++ trees/pathQueriesII.cpp
@@ -1,34 +1,49 @@
 #include <bits/stdc++.h>
 
-
 using namespace std;
 
-int n, m;
-vector<vector<int>> adj;
-vector<int> val, sz, p, depth;
+const int N = 2e5 + 5;
+const int D = 19;
+const int S = (1 << D);
 
-int dfs(int root, int parent) {
-    p[root] = parent;
-    for(int chi : adj[cur]) {
-        if(chi == par) continue;
-        depth[chi] = depth[root] + 1;
-        p[chi] = root;
-        sz[root] += dfs(chi, root);
+int n, q;
+vector<vector<int>> adj, p;
+vector<int> vals, sz, depth, id;
+int st[S];
+
+int dfs_sz(int cur, int par) {
+    for (int chi : adj[cur]) {
+		if (chi == par) continue;
+		depth[chi] = depth[cur] + 1;
+		p[chi][0] = cur;
+		sz[cur] += dfs_sz(chi, cur);
+	}
+	return sz[cur];
+}
+
+void init_lca() {
+	for (int d = 1; d < 18; d++)
+    {	
+        for (int i = 1; i <= n; i++) p[i][d] = p[p[i][d - 1]][d - 1];
     }
-    return sz[root];
 }
 
 int main(){
     cin >> n >> q;
-    adj = vector<vector<int>>(n + 1, vector<int>());
+    vals = vector<int>(n + 1, 0);
     sz = vector<int>(n + 1, 1);
-    sz = vector<int>(n + 1, 0);
     depth = vector<int>(n + 1, 0);
-    for(int i = 0; i <= n; i++) cin >> val[i];
-    for(int i = 0; i <= n; i++) {
+    id = vector<int>(n + 1, 0);
+    adj = vector<vector<int>>(n + 1, vector<int>());
+    p = vector<vector<int>>(n + 1, vector<int>(19, 0));
+    for (int i = 1; i <= n; i++) cin >> vals[i];
+    for (int i = 1; i < n; i++)
+    {
         int a, b;
         cin >> a >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
+	dfs_sz(1, 0);
+	init_lca();
 }
